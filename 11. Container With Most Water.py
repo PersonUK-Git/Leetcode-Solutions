bfb9a6564fc15f66de4b54e4bab229e4b76afb0a
prateek sharma <ppprateeksharma006@gmail.com>
2024-11-08 10:32:20 +0530

    new sol added

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -3,9 +3,10 @@ class Solution:
         res = 0
 
         l, r = 0, len(height) - 1
-        print(r)
+       
         while l < r:
             area = (r - l) * min(height[l], height[r])
+           
             res = max(res, area)
 
             if height[l] < height[r]:
